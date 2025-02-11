e9111646c453a7f1ea443b7c39e3996b78dba0bd
arshiaTests <arshia.org79@gmail.com>
2025-02-12 01:14:16 +0330

    do isEvenOrOdd

--- simple/evenOrOdd.js
+++ simple/evenOrOdd.js
@@ -11,5 +11,10 @@
  */
 
 // Write your solution here
-
+function isEvenOrOdd(num){
+    if(num %2 ==0){
+       return true
+    }else return false
+}
+console.log((isEvenOrOdd(33)))
 module.exports = isEvenOrOdd;
