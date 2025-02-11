4e46bd7211d0fef63e1d9ee6d12caa74931ba823
arshiaTests <arshia.org79@gmail.com>
2025-02-12 01:40:32 +0330

    do sumArray

--- intermediate/sumArray.js
+++ intermediate/sumArray.js
@@ -11,5 +11,14 @@
  */
 
 // Write your solution here
+function sumArray (arr){
+    let sum = 0;
+    for(let i=0 ; i< arr.length;i++){
+        sum+=arr[i]
+    }
+    return sum;
+}
+console.log(sumArray([1, 2, 3])); 
+
 
 module.exports = sumArray;
