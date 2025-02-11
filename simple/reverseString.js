5a5808292a1a2bbeb0eeaddc8078f8c53bb198bc
arshiaTests <arshia.org79@gmail.com>
2025-02-12 00:46:15 +0330

    i think i clone project now

--- simple/reverseString.js
+++ simple/reverseString.js
@@ -11,5 +11,9 @@
  */
 
 // Write your solution here
+function reverseString (str){
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"))
 
 module.exports = reverseString;
--- simple/sumTwoNumbers.js
+++ simple/sumTwoNumbers.js
@@ -14,5 +14,6 @@
 function sum(a, b) {
   return a + b;
 }
+console.log(sum(3,4))
 
 module.exports = sum;
