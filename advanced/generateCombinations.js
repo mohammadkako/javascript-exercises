41e6d17ce45dcebb8546a11899fd1c8a82f0af3a
arshiaTests <arshia.org79@gmail.com>
2025-02-12 20:28:48 +0330

    i use gpt help me

--- advanced/generateCombinations.js
+++ advanced/generateCombinations.js
@@ -10,5 +10,25 @@
  * generateCombinations([1, 2, 3]) should return
  * [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ].
  */
+function generateCombinations(arr) {
+    const result = [];
+    
+    function helper(index, currentCombination) {
+      if (index === arr.length) {
+        if (currentCombination.length > 0) {
+          result.push(currentCombination);
+        }
+        return;
+      }
+      // شامل کردن عنصر فعلی
+      helper(index + 1, [...currentCombination, arr[index]]);
+      // حذف عنصر فعلی
+      helper(index + 1, currentCombination);
+    }
+  
+    helper(0, []);
+    return result;
+  }
+  console.log(generateCombinations([1,2,3]))
 
 module.exports = generateCombinations;
