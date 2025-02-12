ddd4550f3805cb8440f4b4fde85a2db0895ea951
arshiaTests <arshia.org79@gmail.com>
2025-02-13 01:16:20 +0330

    done sum

--- advanced/sumOfDigits.js
+++ advanced/sumOfDigits.js
@@ -9,5 +9,9 @@
  * sumOfDigits(123) should return 6 (1 + 2 + 3).
  * sumOfDigits(987) should return 24 (9 + 8 + 7).
  */
+function sumOfDigits(number) {
+    if (number === 0) return 0;
+    return (number % 10) + sumOfDigits(Math.floor(number / 10));
+}
 
 module.exports = sumOfDigits;
