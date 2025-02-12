686f5281e9bf16efaea1a44361a3220b0aa56477
arshiaTests <arshia.org79@gmail.com>
2025-02-12 19:34:32 +0330

    do average in simple way

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,5 +9,12 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-
+function calculateAverage(arr){
+    let sum = 0;
+    for (let i = 0 ; i<arr.length ; i++){
+        sum+=arr[i];
+    }
+    return sum/arr.length;
+}
+console.log(calculateAverage([1,2,3,4]))
 module.exports = calculateAverage;
