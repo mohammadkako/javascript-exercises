60ad017b4d36266b482ed9ca5ef82fe3316d7eb1
arshiaTests <arshia.org79@gmail.com>
2025-02-13 00:57:42 +0330

    done palindrome

--- advanced/isPalindrome.js
+++ advanced/isPalindrome.js
@@ -12,4 +12,18 @@
  * isPalindrome(10) should return false.
  */
 
+function isPalindrome(num) {
+    // اگر عدد منفی باشد، پالیندروم نیست
+    if (num < 0) {
+      return false;
+    }
+  
+    // تبدیل عدد به رشته برای مقایسه معکوس آن
+    const str = num.toString();
+    const reversedStr = str.split('').reverse().join('');
+  
+    // بررسی اینکه رشته معکوس برابر با رشته اصلی باشد
+    return str === reversedStr;
+  }
+console.log(isPalindrome(121))
 module.exports = isPalindrome;
