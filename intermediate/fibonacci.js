7b14756146f8cfb2a13a45e7d287b7c6c58c2dff
mohammadkako <mehdikako85@gmail.com>
2025-02-16 12:10:37 -0800

    fibonacci was remade

--- intermediate/fibonacci.js
+++ intermediate/fibonacci.js
@@ -11,27 +11,14 @@
  */
 
 // Write your solution here
-function fibonacciRecursive(number) {
-    if(number === 0) { return 0}
-    else if (number === 1) { return 1 }
-    else {
-        return fibonacciRecursive(number-1) + fibonacciRecursive(number-2);
-    }
-}
-function fibonacci(number) {
+function fibonacci(num){
+    let array = [0,1]
+    for (let i = 2 ; i < num ; i++){
+        
+        array.push(array[i-2]+array[i-1])
 
-    if (number === 0) { return []; }
-    else if (number === 1) {return [0]}
-    else if (number === 2) {return [0,1]}
-    else {
-        let arr = new Array(number);
-        arr[0] = 0
-        arr[1] = 1;
-        for (let i = 2; i < arr.length; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2] ;
-        }
-        return arr;
     }
+  return array
 }
 
 module.exports = fibonacci;
