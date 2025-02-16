912db340733100e559b93145dd09e3d2b5c433ff
mohammadkako <mehdikako85@gmail.com>
2025-02-16 12:09:35 -0800

    isPrime was refactord

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -12,12 +12,12 @@
 
 // Write your solution here
 const isPrime = (number ) => {
-    if (number < 2 ) { return false }
-    if (number === 2) return true;
-    let max = Math.floor(number ** 0.5) + 1 ;
-    for (let i = 2; i <= max; i++) {
-        if (number % i === 0  ) {return false;}
+    if(number === 2 || number === 3){
+        return true
+    }else if (number % 2 === 0 || number % 3 === 0 || number === 1 || number === 0){
+        return false
+    }else{
+        return true
     }
-    return true;
 }
 module.exports = isPrime;
