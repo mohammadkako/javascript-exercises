317906c7063c30cbc5435f8ce21a5eb84cd23bd8
mohammadkako <mehdikako85@gmail.com>
2025-02-18 12:07:07 -0800

    calculate average was remade

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,9 +9,6 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-function calculateAverage(num , sum = 0 , index = 0){
-    if (index === num.length) return sum / num.length;
-    return calculateAverage(num , sum+num[index],index+1)
-}
-console.log(calculateAverage([1,2,3,4]))
+const calculateAverage = (array) => array.length === 0 ? NaN :
+(array.reduce((total,value) => total + value))/array.length
 module.exports = calculateAverage;
