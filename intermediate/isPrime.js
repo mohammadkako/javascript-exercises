e9eacd8d6278ea125cc3df1e4e8c4a7ebf52d226
mohammadkako <mehdikako85@gmail.com>
2025-02-18 11:54:38 -0800

    is prime  was remade

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -11,15 +11,7 @@
  */
 
 // Write your solution here
-const isPrime = (num) => {
-    if (num <= 1) return false; 
-    if (num === 2) return true; 
-    for (let i = 2; i < num; i++) { 
-        if (num % i === 0) return false; 
-    }
-    return true; 
-};
-
-console.log(isPrime(5)); 
+const isPrime = (num) => num < 2 || (num % 2 === 0 && num !==2) ||(num % 3 === 0 && num !==3) ? false 
+    : true
 
 module.exports = isPrime;
